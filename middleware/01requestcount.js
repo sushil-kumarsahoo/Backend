2407ef18977b8d0d63548ddef18fcf28e35f8b46
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2025-12-11 23:58:06 +0530

    assignment

--- middleware/01requestcount.js
+++ middleware/01requestcount.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 
 let requestCount = 0;
+
+//you have been given an express server which has few endpoints your tsk is to create a global middleware which will mintain the count of total requests made to the server  in the global variable 
  
 app.use(function(req,res,next){
     requestCount = requestCount + 1;
