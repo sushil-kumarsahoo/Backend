f00aa2d303d2f51deac1cd56add71cff17192f22
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-02-09 00:54:11 +0530

    prop drilling,context api

---
+++ prop-drilling/src/App.jsx
@@ -0,0 +1,52 @@
+import { useContext, useState } from "react";
+import "./App.css";
+import { CountContext } from "./context";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <CountContext.Provider value={{ count, setCount }}>
+        <Count/>
+      </CountContext.Provider>
+    </div>
+  );
+}
+
+function Count() {
+  return (
+    <div>
+      <CountRender />
+      <Button />
+    </div>
+  );
+}
+function CountRender() {
+const {count} = useContext(CountContext);
+  return <div>{count}</div>;
+}
+
+function Button() {
+  const { count, setCount } = useContext(CountContext);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrease
+      </button>
+    </div>
+  );
+}
+
+export default App;
